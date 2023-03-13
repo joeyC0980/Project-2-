cdd5bdf792c17099fd6e66c5c36c2280caf751e2
Joey Cheng <joeycheng2080@gmail.com>
2023-03-13 17:30:14 -0400

    updated again 2

--- server.js
+++ server.js
@@ -29,15 +29,19 @@ app.use(session({
 const mongoose= require("mongoose")
 mongoose.connect(process.env.DATABASE_URI);
 
+const MONGODB_URI = process.env.MONGODB_URI;
+mongoose.connect(MONGODB_URI , { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+	);
+
 
 const db = mongoose.connection
 db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
-db.on('connected', () => console.log('mongo connected'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
 db.on('disconnected', () => console.log('mongo disconnected'));
 
 //Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 
 
